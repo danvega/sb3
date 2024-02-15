80004d586a0e73a30c2265bbab3de56fbd04a82c
Dan Vega <danvega@gmail.com>
2024-02-15 12:03:18 -0500

    Demo Starting Branch

--- src/main/java/dev/danvega/sb3/Application.java
+++ src/main/java/dev/danvega/sb3/Application.java
@@ -1,16 +1,11 @@
 package dev.danvega.sb3;
 
-import dev.danvega.sb3.post.Post;
-import dev.danvega.sb3.post.PostClient;
-import dev.danvega.sb3.post.PostRepository;
-import jakarta.servlet.ServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -26,18 +21,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	PostClient postClient() {
-		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
-		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
-		return factory.createClient(PostClient.class);
-	}
-
-	@Bean
-	CommandLineRunner commandLineRunner(PostRepository postRepository, PostClient postClient) {
-		return args -> {
-			List<Post> posts = postClient.findAll();
-			postRepository.saveAll(posts);
-		};
-	}
 }
